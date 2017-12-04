9073649dc748e8920f6a8357b5491255f25bbecd
XiajZhang <Brayden@zhangxiajiedeMacBook-Pro.local>
2017-12-04 18:43:04 -0500

    changed
    changed to two servers.
    1. port 3000 for store data
    2. port 4200 for retrieve data.

---
+++ i-community/src/app/question/question.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { QuestionsService } from '../questions.service';
+import { Query } from '@angular/core/src/metadata/di';
+
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
+})
+export class QuestionComponent implements OnInit {
+
+  questions: any = [];
+  newQuestion: any;
+  // selectedQuestion: any;
+
+  constructor(private questionsService: QuestionsService) { }
+
+  ngOnInit() {
+    this.questionsService.getAllQuestions().subscribe(questions =>
+      {
+        console.log(questions);
+        this.questions = questions; 
+        this.sortOnTime();
+    });
+    // this.questionsService.getQuestions().then( questions=>{
+    //   this.questions = questions;
+    // })
+  }
+
+  sortOnTime(){
+    this.questions.sort(function(a, b){
+      return new Date(b.created_date) > new Date(a.created_date);
+    });
+  }
+
+  postQuestion(){
+    var response = '{"title" : "' + this.newQuestion + '"}';
+    console.log(response);
+    this.questionsService.postQuestion(JSON.parse(response));
+    
+    // console.log(this.questions);
+    // this.questionsService.getAllQuestions().subscribe(questions =>
+    //   { this.questions = questions; 
+    // });
+    // console.log(this.questions);
+    window.location.reload();
+  }
+
+}
---
+++ icommunity-server/server/controllers/question-controller.js
@@ -0,0 +1,77 @@
+/**
+ * Controller for question endpoints.
+ */
+
+'use strict';
+//import question service.
+const questionService = require('../services/question-service');
+
+/**
+ * Returns a list of questions in JSON based on the
+ * search parameters.
+ *
+ * @param {request} {HTTP request object}
+ * @param {response} {HTTP response object}
+ */
+exports.list = function (request, response) {
+    questionService.search({}, function (questions) {
+        response.status(200);
+        response.json(questions);
+    });
+};
+
+/**
+ * Creates a new question with the request JSON and
+ * returns question JSON object.
+ *
+ * @param {request} {HTTP request object}
+ * @param {response} {HTTP response object}
+ */
+exports.post = function (request, response) {
+    let newQuestion = Object.assign({}, request.body);
+    questionService.save(newQuestion, function (question) {
+        response.json(question);
+    });
+};
+
+/**
+ * Returns a question object in JSON.
+ *
+ * @param {request} {HTTP request object}
+ * @param {response} {HTTP response object}
+ */
+exports.get = function (request, response) {
+    questionService.get(request.params.questionId, function (question) {
+        response.json(question);
+    });
+};
+
+/**
+ * Updates and returns a question object in JSON.
+ *
+ * @param {request} {HTTP request object}
+ * @param {response} {HTTP response object}
+ */
+exports.put = function (request, response) {
+    let question = Object.assign({}, request.body);
+    question._id = request.params.questionId;
+    // console.log(question);
+    // console.log(question.answers);
+    questionService.update(question, function (question) {
+        response.json(question);
+    });
+};
+
+/**
+ * Deletes a question object.
+ *
+ * @param {request} {HTTP request object}
+ * @param {response} {HTTP response object}
+ */
+exports.delete = function (request, response) {
+    questionService.delete(request.params.questionId, function (question) {
+        response.json({
+            message: 'question Successfully deleted'
+        });
+    });
+};
