6ed4eb1bfda2ec5eec933ed617455913a1385e10
Rohit Naik <naik.roh@husky.neu.edu>
2017-12-07 17:24:49 -0500

    adding notice works

--- iCommunity/client/src/app/notice-edit/notice-edit.component.ts
+++ iCommunity/client/src/app/notice-edit/notice-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { UserService,AlertService } from '../services/index';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-notice-edit',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoticeEditComponent implements OnInit {
 
-  constructor() { }
+  currentUser: User;
+  tempemail:String;
+  model:any={};
 
-  ngOnInit() {
+  constructor(private userService: UserService,
+      private router: Router,
+      private alertService: AlertService) {
+      this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+      this.model = this.currentUser;
   }
 
+  ngOnInit() {
+      // this.loadAllUsers();
+  }
+  loading = false;
+
+  update() {
+    this.model._id = this.currentUser._id;
+    this.loading = true;
+    this.userService.update(this.model)
+        .subscribe(
+            data => {
+                this.alertService.success('Update successful', true);
+                this.router.navigate(['/home']);
+            },
+            error => {
+                this.alertService.error(error);
+                this.loading = false;
+            });
+            
+}
+  
 }
