43f33407ecab586304d6acbdcdf7c242e7a7d427
XiajZhang <Brayden@zhangxiajiedeMacBook-Pro.local>
2017-12-10 17:08:12 -0500

    navbar
    navbar

--- iCommunity/client/src/app/login/login.component.ts
+++ iCommunity/client/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { AlertService, UserService,AuthenticationService } from '../services/index';
+import { window } from 'rxjs/operators/window';
 
 @Component({
     moduleId: module.id,
@@ -23,8 +24,6 @@ export class LoginComponent implements OnInit {
 
     ngOnInit() {
         // reset login status
-        this.authenticationService.logout();
-
         // get return url from route parameters or default to '/'
         // this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/questions';
         // this.returnUrl = '/home';
